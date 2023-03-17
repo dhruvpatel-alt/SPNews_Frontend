b5487942c3b3e06ac6ed5a1aec7c38f9455460e6
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-03-17 16:45:41 +0530

    Update Spinner.js

--- src/component/Spinner.js
+++ src/component/Spinner.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function Spinner() {
     return (
-        div class="container d-flex justify-content-center">
+       < div class="container d-flex justify-content-center">
      <div class="spinner-border" role="status">
   <span class="sr-only">Loading...</span>
 </div>
