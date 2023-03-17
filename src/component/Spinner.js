89ba7d34e721588f2a42db628d655f037c2941b2
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-03-17 16:41:30 +0530

    Update Spinner.js

--- src/component/Spinner.js
+++ src/component/Spinner.js
@@ -2,8 +2,8 @@ import React from 'react'
 import loading from './loading.gif'
 export default function Spinner() {
     return (
-        <div className="text-center" style={{display:"flex",justifyContent:"center" ,left:"30px"}}>
-            <img src={loading} alt="loading"></img>
-        </div>
+     <div class="spinner-border" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
     )
 }
