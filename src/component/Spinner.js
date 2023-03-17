bf258e93d209df9ef22800206854cd3f2d3bbbdd
DHRUV PATEL <69008045+dhruvpatel-alt@users.noreply.github.com>
2023-03-17 16:44:49 +0530

    Update Spinner.js

--- src/component/Spinner.js
+++ src/component/Spinner.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import loading from './loading.gif'
 export default function Spinner() {
     return (
+        div class="container d-flex justify-content-center">
      <div class="spinner-border" role="status">
   <span class="sr-only">Loading...</span>
 </div>
+      </div>  
     )
 }
